6d7ed5f8b34791f1b8531982731572db7b9ddf98
Simon Foster <simondf@gmail.com>
2026-09-14 17:06:15 +0100

    One final go at fixing this

--- Carnap/src/Carnap/Languages/HOArithSum/Logic/FosterLaursen.hs
+++ Carnap/src/Carnap/Languages/HOArithSum/Logic/FosterLaursen.hs
@@ -207,7 +207,7 @@ instance Inference HOArithSumFL HOArithSumLex (Form Bool) where
     indirectInference (TFL x) = indirectInference x
     indirectInference Induction     = Just assumptiveProof
     indirectInference InductionPlus = Just assumptiveProof
-    indirectInference (EqChain n)  = Just (TypedProof (ProofType 0 n))
+    indirectInference (EqChain n) = Just (PolyTypedProof n (ProofType 0 1))
     indirectInference x
         | x `elem` [EE1, EE2] = Just assumptiveProof
         | otherwise = Nothing
